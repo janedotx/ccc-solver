0c346f1b2cf846b433ae51a52875e594034a3b4f
Kairen Wong <ithekai@users.noreply.github.com>
2020-03-19 01:01:42 -0700

    add mocha add tests for length, validity, fairness
    add basic tests

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-const solver = require('javascript-lp-solver')
+const solver = require("javascript-lp-solver");
 
 function shuffle(a) {
-        var j, x, i;
-        for (i = a.length - 1; i > 0; i--) {
-            j = Math.floor(Math.random() * (i + 1));
-            x = a[i];
-            a[i] = a[j];
-            a[j] = x;
-        }
-        return a;
+    for (let i = a.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let x = a[i];
+        a[i] = a[j];
+        a[j] = x;
     }
+    return a;
+}
 
 function computeSchedule(nworkers, workerAvailability) {
-    console.log('workers: ', nworkers)
-    console.log('workeravailability: ', workerAvailability)
-    var JSLACK_MAX = 7;
+    //console.log('workers: ', nworkers)
+    //console.log('workeravailability: ', workerAvailability)
+
+    const JSLACK_MAX = 7;
 
     /* construct a model to be solved later */
     var model = {
-        "optimize": "unfairness", // objective is to minimize unfairness
-        "opType": "min",
-        "constraints": {},
-        "variables": {},
-        "ints": {}
+        optimize: "unfairness", // objective is to minimize unfairness
+        opType: "min",
+        constraints: {},
+        variables: {},
+        ints: {}
     };
 
-    var ardays = []
+    var ardays = [];
     for (iday = 0; iday < workerAvailability.length; iday += 1) {
         ardays.push(iday);
     }
     shuffle(ardays); //shuffle for better randomization
 
     var ndays = 0; // tracks total days currently entered in optimization problem, increments each time a new day is added
-                   // note that days with no availability for any worker will not be added into the solver
+    // note that days with no availability for any worker will not be added into the solver
     for (iday of ardays) {
-        day = workerAvailability[iday]
+        day = workerAvailability[iday];
         day = shuffle(day); //shuffle the workers in the days
-        for (var iworker=0; iworker<day.length; iworker++) {
-            availableWorker = day[iworker]
+        for (var iworker = 0; iworker < day.length; iworker++) {
+            availableWorker = day[iworker];
             //create a variable like 'assignedp2d13' which will be 1 if the parent is assigned that day, 0 otherwsie
             let vn = "assignedw" + availableWorker + "d" + iday; // 'assignedp2d13'
             let vns = "aw" + availableWorker + "d" + iday; // a short name of the variable, used to constrain it to be non-negative
@@ -51,29 +51,29 @@ function computeSchedule(nworkers, workerAvailability) {
             model.variables[vn][vw_max] = 1; //will set relevant constraint later
             model.variables[vn][vw_min] = 1; //will set relevant constraint later
             model.variables[vn][vd] = 1;
-            model.constraints[vns] = {"min": 0}; // constrain that this work variable is non-negative
+            model.constraints[vns] = { min: 0 }; // constrain that this work variable is non-negative
             model.ints[vn] = 1; // specify that this variable should be an integer
 
-            if (!(model.constraints.hasOwnProperty(vd))) { // if we haven't seen this day yet
-                model.constraints[vd] = {"equal": 1}; // constrain that the number of workers that work this day is exactly 1
+            if (!model.constraints.hasOwnProperty(vd)) {
+                // if we haven't seen this day yet
+                model.constraints[vd] = { equal: 1 }; // constrain that the number of workers that work this day is exactly 1
                 ndays += 1; // and add 1 to our counter for the total number of days in the problem
-
             }
         }
     }
 
-    let fairalloc = ndays/nworkers;
+    let fairalloc = ndays / nworkers;
     let fairallocround = Math.round(fairalloc);
 
     /* for each worker */
-    minmaxar = ["min", "max"]
+    minmaxar = ["min", "max"];
     for (c of minmaxar) {
         for (iworker = 0; iworker < nworkers; iworker += 1) {
-            let vw =  c + "totalw" + iworker;
-            for (var jslack=0; jslack<JSLACK_MAX; jslack+=1) {
-                let sw =  c + "slackw" + iworker + "index" + jslack; // a "slack" variable for the worker
-                let sw_max =  c + "swmax" + iworker + "index" + jslack; //constraint variable for max on slack var
-                let sw_min =  c + "swmin" + iworker + "index" + jslack; //min
+            let vw = c + "totalw" + iworker;
+            for (var jslack = 0; jslack < JSLACK_MAX; jslack += 1) {
+                let sw = c + "slackw" + iworker + "index" + jslack; // a "slack" variable for the worker
+                let sw_max = c + "swmax" + iworker + "index" + jslack; //constraint variable for max on slack var
+                let sw_min = c + "swmin" + iworker + "index" + jslack; //min
 
                 model.variables[sw] = {}; // add the slack variable
                 model.variables[sw][sw_max] = 1;
@@ -86,38 +86,42 @@ function computeSchedule(nworkers, workerAvailability) {
                 }
 
                 if (c == "max") {
-                    model.variables[sw]["unfairness"] = Math.pow(fairallocround + jslack + 1 - fairalloc, 2) - Math.pow(fairallocround + jslack - fairalloc, 2);
+                    model.variables[sw]["unfairness"] =
+                        Math.pow(fairallocround + jslack + 1 - fairalloc, 2) -
+                        Math.pow(fairallocround + jslack - fairalloc, 2);
                 } else {
-                    model.variables[sw]["unfairness"] = Math.pow(fairallocround - jslack - 1 - fairalloc,2) - Math.pow(fairallocround - jslack - fairalloc,2);
+                    model.variables[sw]["unfairness"] =
+                        Math.pow(fairallocround - jslack - 1 - fairalloc, 2) -
+                        Math.pow(fairallocround - jslack - fairalloc, 2);
                 }
-                if (jslack != JSLACK_MAX-1) {
-                    model.constraints[sw_max] = {"max": 1}; //constrain to less (or equal to) than 1
+                if (jslack != JSLACK_MAX - 1) {
+                    model.constraints[sw_max] = { max: 1 }; //constrain to less (or equal to) than 1
                 }
-                model.constraints[sw_min] = {"min": 0};  //constrain to more (or equal to) than 0
-           }
-           if (c == "min") {
-                model.constraints[vw] = {"min": fairallocround};
-           } else {
-                model.constraints[vw] = {"max": fairallocround};
-           }
+                model.constraints[sw_min] = { min: 0 }; //constrain to more (or equal to) than 0
+            }
+            if (c == "min") {
+                model.constraints[vw] = { min: fairallocround };
+            } else {
+                model.constraints[vw] = { max: fairallocround };
+            }
         }
     }
 
     /* log some stuff to the console, for debugging */
-    console.log("model:", model);
+    //console.log("model:", model);
 
     /* solve the problem */
     var results = solver.Solve(model);
-    console.log("solved: ", results);
+    //console.log("solved: ", results);
 
-    let outputs = []
-    for (iday = 0; iday<workerAvailability.length; iday+=1) {
+    let outputs = [];
+    for (iday = 0; iday < workerAvailability.length; iday += 1) {
         outputs[iday] = -1; //initialize assignment to 0
     }
     if (results.feasible) {
         /* for each day, assign it the appropriate classes for its assignment */
         for (iday = 0; iday < workerAvailability.length; iday += 1) {
-            for (iworker = 0; iworker <nworkers; iworker+=1 ) {
+            for (iworker = 0; iworker < nworkers; iworker += 1) {
                 let vn = "assignedw" + iworker + "d" + iday;
                 if (results[vn] == 1) {
                     outputs[iday] = iworker;
@@ -125,12 +129,13 @@ function computeSchedule(nworkers, workerAvailability) {
                 }
             }
         }
-        console.log("assignments: ", outputs);
+        //console.log("assignments: ", outputs);
     } else {
-        console.log("No assignment possible due to solver error.")
+        console.log("No assignment possible due to solver error.");
+        throw "No assignment possible due to solver error";
         return [null, 0];
     }
     return [outputs, results.result];
 }
 
-exports.computeSchedule = computeSchedule
+exports.computeSchedule = computeSchedule;
---
+++ test/test.js
@@ -0,0 +1,316 @@
+const scheduler = require("../index.js");
+var assert = require("assert");
+
+//lists availability by worker instead of by day
+function createAvailabilityByWorker(nworkers, availabilityByDay) {
+    var result = [];
+    var ndays = availabilityByDay.length;
+    for (let iworker = 0; iworker < nworkers; iworker++) {
+        result.push([]); //make new array for that worker
+        for (let iday = 0; iday < ndays; iday++) {
+            if (availabilityByDay[iday].includes(iworker)) {
+                result[iworker].push(iday); //if assigned, push to worker's array
+            }
+        }
+    }
+    return result;
+}
+
+//lists assignments by worker instead of by day
+function createAssignmentsByWorker(nworkers, assignmentsByDay) {
+    var result = [];
+    var ndays = assignmentsByDay.length;
+    for (let iworker = 0; iworker < nworkers; iworker++) {
+        result.push([]); //make new array for that worker
+        for (let iday = 0; iday < ndays; iday++) {
+            if (assignmentsByDay[iday] == iworker) {
+                result[iworker].push(iday); //if assigned, push to worker's array
+            }
+        }
+    }
+    return result;
+}
+
+//determines if an assignment is valid by availability, returns true or false
+function isValidAssignment(
+    nworkers,
+    availabilityByWorker,
+    assignmentsByWorker
+) {
+    for (let iworker = 0; iworker < nworkers; iworker++) {
+        for (let iassign of assignmentsByWorker[iworker]) {
+            if (!availabilityByWorker[iworker].includes(iassign)) {
+                throw "Worker " +
+                    iworker +
+                    " was assigned Day " +
+                    iassign +
+                    " despite unavailability.";
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+//deteremines if an assigment is "fair" by checking possible swaps which would improve fairness
+function isFairAssignment(
+    nworkers,
+    availabilityByDay,
+    assignmentsByDay,
+    assignmentsByWorker
+) {
+    for (let iday = 0; iday < assignmentsByDay.length; iday++) {
+        let assignedWorker = assignmentsByDay[iday];
+        let availableWorkers = availabilityByDay[iday];
+        for (let availableWorker of availableWorkers) {
+            if (availableWorker != assignedWorker) {
+                //can't swap with yourself
+                if (
+                    assignmentsByWorker[availableWorker].length <
+                    assignmentsByWorker[assignedWorker].length - 1
+                ) {
+                    throw "Day " +
+                        iday +
+                        " assigned to Worker " +
+                        assignedWorker +
+                        " even though Worker " +
+                        availableWorker +
+                        " had sufficiently lower total assigned workload.";
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+function makeRandomAvailability(nworkers, ndays, c) {
+    var result = [];
+    for (let iday = 0; iday < ndays; iday++) {
+        result.push([]);
+        for (let iworker = 0; iworker < nworkers; iworker++) {
+            if (Math.random() < c) {
+                result[result.length - 1].push(iworker);
+            }
+        }
+    }
+    return result;
+}
+
+function makeAvailabilityByProbabilityArray(ndays, availProbabilityArray) {
+    var result = [];
+    var nworkers = availProbabilityArray.length;
+    for (let iday = 0; iday < ndays; iday++) {
+        result.push([]);
+        for (let iworker = 0; iworker < nworkers; iworker++) {
+            if (Math.random() < availProbabilityArray[iworker]) {
+                result[result.length - 1].push(iworker);
+            }
+        }
+    }
+    return result;
+}
+
+function makeOneWorkerOffSyncAvailability(nworkers, ndays, daysOffSync, c) {
+    var result = [];
+    for (let iday = 0; iday < ndays; iday++) {
+        result.push([]);
+    }
+    var randomWorker = Math.floor(Math.random() * (nworkers - 1));
+    for (let iday = 0; iday < daysOffSync; iday++) {
+        result[iday].push(randomWorker);
+    }
+    for (let iday = daysOffSync; iday < ndays; iday++) {
+        for (let iworker = 0; iworker < nworkers; iworker++) {
+            if (iworker != randomWorker) {
+                if (Math.random() < c) {
+                    result[iday].push(iworker);
+                }
+            }
+        }
+    }
+    return result;
+}
+
+function testAssignment(
+    description,
+    nworkers,
+    availabilityByDay,
+    assignmentsByDay
+) {
+    assignment = scheduler.computeSchedule(nworkers, availabilityByDay);
+    assignmentsByDay = assignment[0];
+    let availabilityByWorker = createAvailabilityByWorker(
+        nworkers,
+        availabilityByDay
+    );
+    let assignmentsByWorker = createAssignmentsByWorker(
+        nworkers,
+        assignmentsByDay
+    );
+
+    testAssignmentLength(description, availabilityByDay, assignmentsByDay);
+    testAssignmentValidity(
+        description,
+        nworkers,
+        availabilityByWorker,
+        assignmentsByWorker
+    );
+    testAssignmentFairnessUsingSwaps(
+        description,
+        nworkers,
+        availabilityByDay,
+        assignmentsByDay,
+        assignmentsByWorker
+    );
+}
+
+describe("scheduler.computeSchedule()", function() {
+    var tests = [
+        {
+            name: "Test Case: 01 Person, 01 Days, All Available",
+            input: {
+                nworkers: 1,
+                availabilityByDay: [[0]]
+            }
+        },
+        {
+            name: "Test Case: 10 People, 09 Days, No Availability",
+            input: {
+                nworkers: 10,
+                availabilityByDay: [[], [], [], [], [], [], [], [], []]
+            }
+        },
+        {
+            name: "Test Case: 05 People, 20 Days, All Available",
+            input: {
+                nworkers: 5,
+                availabilityByDay: [
+                    [3, 0, 2, 1, 4],
+                    [3, 4, 2, 0, 1],
+                    [1, 2, 0, 3, 4],
+                    [2, 0, 4, 3, 1],
+                    [3, 4, 2, 0, 1],
+                    [1, 2, 3, 0, 4],
+                    [2, 0, 4, 1, 3],
+                    [2, 0, 3, 1, 4],
+                    [2, 1, 4, 3, 0],
+                    [0, 3, 4, 2, 1],
+                    [0, 3, 1, 4, 2],
+                    [1, 3, 2, 0, 4],
+                    [2, 4, 3, 1, 0],
+                    [1, 4, 3, 0, 2],
+                    [4, 0, 2, 1, 3],
+                    [4, 0, 2, 3, 1],
+                    [3, 1, 2, 4, 0],
+                    [4, 1, 3, 2, 0],
+                    [0, 4, 2, 1, 3],
+                    [1, 2, 0, 3, 4]
+                ]
+            }
+        },
+        {
+            name: "Test Case: 05 People, 20 Days, .7 Availability",
+            input: {
+                nworkers: 5,
+                availabilityByDay: makeRandomAvailability(5, 20, 0.7)
+            }
+        },
+        {
+            name: "Test Case: 03 People, 31 Days, .2 Availability",
+            input: {
+                nworkers: 3,
+                availabilityByDay: makeRandomAvailability(3, 31, 0.2)
+            }
+        },
+        {
+            name: "Test Case: 06 People, 27 Days, One Person Very Available",
+            input: {
+                nworkers: 6,
+                availabilityByDay: makeAvailabilityByProbabilityArray(27, [
+                    0.3,
+                    0.3,
+                    0.9,
+                    0.3,
+                    0.3,
+                    0.3
+                ])
+            }
+        },
+        {
+            name: "Test Case: 04 People, 34 Days, One Person Very Unavailable",
+            input: {
+                nworkers: 4,
+                availabilityByDay: makeAvailabilityByProbabilityArray(34, [
+                    0.4,
+                    0.4,
+                    0.05,
+                    0.4
+                ])
+            }
+        },
+        {
+            name: "Test Case: 07 People, 50 Days, One Person Off Sync",
+            input: {
+                nworkers: 7,
+                availabilityByDay: makeOneWorkerOffSyncAvailability(
+                    7,
+                    50,
+                    17,
+                    0.3
+                )
+            }
+        }
+    ];
+
+    tests.forEach(function(test) {
+        //console.log(test.name);
+        //console.log(test.input.availabilityByDay);
+        var assignment = scheduler.computeSchedule(
+            test.input.nworkers,
+            test.input.availabilityByDay
+        );
+        //assignemnt is an array, the first part of which is an assignment,
+        // the second part of which is an "unfairness" score
+        var assignmentsByDay = assignment[0];
+        var assignmentsByWorker = createAssignmentsByWorker(
+            test.input.nworkers,
+            assignmentsByDay
+        );
+        //console.log("assignments by worker:")
+        //console.log(assignmentsByWorker);
+
+        var availabilityByWorker = createAvailabilityByWorker(
+            test.input.nworkers,
+            test.input.availabilityByDay
+        );
+
+        it(test.name + " has correct length", function() {
+            assert.equal(
+                assignmentsByDay.length,
+                test.input.availabilityByDay.length
+            );
+        });
+
+        it(test.name + " has valid assignemnt", function() {
+            assert(
+                isValidAssignment(
+                    test.input.nworkers,
+                    availabilityByWorker,
+                    assignmentsByWorker
+                )
+            );
+        });
+
+        it(test.name + " has fair assignemnt", function() {
+            assert(
+                isFairAssignment(
+                    test.input.nworkers,
+                    test.input.availabilityByDay,
+                    assignmentsByDay,
+                    assignmentsByWorker
+                )
+            );
+        });
+    });
+});
